f79d628831950c2d27fc574db4981342a7090d4c
mukarama-farouk <faroukmukarama@gmail.com>
2023-11-16 13:43:41 +0000

    update

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -8,6 +8,6 @@ exec { 'fix--for-nginx':
 
 # Restart Nginx
 exec { 'nginx-restart':
-  command     => 'nginx restart',
-  path        => '/etc/init.d/',
+  command => 'nginx restart',
+  path    => '/etc/init.d/',
 }
